9a965fbb8f2ed0b00eada7598212cff0aa872f64
Alex Garrett-Smith <alex@codecourse.com>
2024-06-21 11:20:37 +0100

    Episode 09

--- resources/views/livewire/chat/messages.blade.php
+++ resources/views/livewire/chat/messages.blade.php
@@ -1,7 +1,9 @@
 <div class="h-64 overflow-y-scroll border border-gray-200 rounded-lg px-4 py-3">
-    <div class="space-y-4 pb-4">
+    <div class="sm:[overflow-anchor:none] space-y-4 pb-4">
         @foreach($messages as $message)
             <livewire:chat.message :message="$message" :key="$message->id" />
         @endforeach
     </div>
+
+    <div class="sm:[overflow-anchor:auto] h-px" x-init="$el.scrollIntoView()"></div>
 </div>
