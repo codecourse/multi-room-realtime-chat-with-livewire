a51315a63380930b792e6f10983de6c38264323e
Alex Garrett-Smith <alex@codecourse.com>
2024-06-24 15:41:41 +0100

    Episode 14

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,24 @@ class Message extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function createdAtHuman(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->getCreatedAtHumanDate()
+        );
+    }
+
+    protected function getCreatedAtHumanDate()
+    {
+        $day = match(true) {
+            $this->created_at->isToday() => 'Today',
+            $this->created_at->isYesterday() => 'Yesterday',
+            default => $this->created_at->toDateString(),
+        };
+
+        return $day . ' ' . $this->created_at->toTimeString('minute');
+    }
 
     public function user()
     {
--- resources/views/livewire/chat/message.blade.php
+++ resources/views/livewire/chat/message.blade.php
@@ -3,7 +3,7 @@
     <div>
         <div class="flex items-baseline space-x-2">
             <div class="font-bold">{{ $message->user->name }}</div>
-            <span class="text-gray-600 text-xs">{{ $message->created_at }}</span>
+            <span class="text-gray-600 text-xs">{{ $message->created_at_human }}</span>
         </div>
         <p>{{ $message->body }}</p>
     </div>
